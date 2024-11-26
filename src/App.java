c2d049be0eec8a689149c794c2252c307cb08947
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-11-26 21:19:13 +0000

    feat: experiment one completed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import com.salesianoslacuesta.resources.Crop;
+import com.salesianoslacuesta.threads.Client;
+import com.salesianoslacuesta.threads.Farmer;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Crop crop = new Crop();
+
+        
+        Farmer farmerr = new Farmer("luis", 5, crop);
+        Client client = new Client("Pepe", 5, crop);
+
+        farmerr.start();
+        client.start();
+
+        farmerr.join();
+        client.join();
+
+
+        
+
+
     }
+
 }
